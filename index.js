f54b67ffcba1f869ad12e6cbf3e49b292b321726
zelconsulting <Zelconsulting@gmail.com>
2025-09-27 02:26:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const fetch = require('node-fetch');
-const xml2js = require('xml2js');
+import fetch from 'node-fetch';
+import { Parser } from 'xml2js';
+import fs from 'fs';
 
 const CORS_PROXY = 'https://api.allorigins.win/raw?url=';
 const foxNewsRSS = 'http://feeds.foxnews.com/foxnews/latest';
 const cnnNewsRSS = 'http://rss.cnn.com/rss/cnn_topstories.rss';
 
-const parser = new xml2js.Parser({ explicitArray: false });
+const parser = new Parser({ explicitArray: false });
 
 async function getHeadlines(url) {
     try {
@@ -33,7 +34,6 @@ async function generateHeadlinesJson() {
         timestamp: new Date().toISOString()
     };
     
-    const fs = require('fs');
     fs.writeFileSync('headlines.json', JSON.stringify(data, null, 2));
     console.log('Successfully generated headlines.json');
 }
