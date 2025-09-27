fb52ff98080f1c5a6cfeff3417cb399a4ea8bc5f
zelconsulting <Zelconsulting@gmail.com>
2025-09-27 08:14:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { Parser } from 'xml2js';
 import fs from 'fs';
 
 const CORS_PROXY = 'https://corsproxy.io/?';
-const foxNewsRSS = 'https://moxie.foxnews.com/google-publisher/latest.xml';
+const foxNewsRSS = 'https://rss.app/feeds/2oA27s1Fj2G2280d.xml';
 const cnnNewsRSS = 'https://rss.app/feeds/gaoNnoqPkA5O5oc6.xml';
 
 const parser = new Parser({ explicitArray: false });
