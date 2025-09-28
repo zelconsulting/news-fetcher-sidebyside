c04e5afeb08047ea7d447eb9742d0a30d1074547
zelconsulting <Zelconsulting@gmail.com>
2025-09-27 20:28:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import fs from 'fs';
 const CORS_PROXY = 'https://corsproxy.io/?';
 const foxNewsRSS = 'http://feeds.feedburner.com/FoxNews/Latest';
 const foxNewsPoliticsRSS = 'https://moxie.foxnews.com/google-publisher/politics.xml';
-const cnnNewsRSS = 'http://rss.cnn.com/rss/cnn_topstories.rss';
-const cnnPoliticsRSS = 'http://rss.cnn.com/rss/cnn_allpolitics.rss';
+const cnnNewsRSS = 'https://rss.app/feeds/gaoNnoqPkA5O5oc6.xml';
+const cnnPoliticsRSS = 'https://rss.app/feeds/ZV5iFmOIc8kONdND.xml';
 const newFeed = 'https://rss.app/feeds/6kYSPr4nshuhgT5w.xml'; // Added new feed
 
 // Keywords to filter for politically-charged headlines
